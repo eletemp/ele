8b3101161832d580fd1c49c00f8012471fa5d58f
shufenHou <2485408442@qq.com>
2018-06-03 01:59:02 +0800

    商铺页面数据展示

--- ele.js
+++ ele.js
@@ -6,12 +6,17 @@ app.use("/",express.static("webapp"));
 //把模板注入到app中
 app.set("view engine","ejs");
 app.set("views","webapp");
+//首页渲染
+app.get("/",function (req,res) {
+    res.render("index.ejs");
+})
+app.get("/favicon.ico",function(req,res){
+    return;
+})
 //-------路由---------
-require("./route/homeRoute")(app);
-
+require("./route/UserRoute")(app);
 
 //-------监听端口-------
 app.listen(9090,function(err){
     if(err) throw err;
-    console.log("服务器启动成功！");
 });
