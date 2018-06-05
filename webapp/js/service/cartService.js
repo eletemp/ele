1a65e74a7d6728f1a5083ee7c8f8446328f80ff3
jojoliny <370795317@qq.com>
2018-06-06 01:17:41 +0800

    商店食品列表页面的购物车完成

---
+++ webapp/js/service/cartService.js
@@ -0,0 +1,28 @@
+(function (angular) {
+    var app=angular.module("ele.service.cart",[]);
+    app.service("cart",function () {
+        var cartItem={};
+        this.getCartItem=function () {
+            return cartItem;
+        }
+        this.addItem=function (food_id,food_name,food_price) {
+            if(cartItem[food_id]==undefined){
+                cartItem[food_id]={
+                    food_name:food_name,
+                    food_price:food_price,
+                    quantity:1
+                };
+            }
+        }
+        this.add=function (food_id) {
+            cartItem[food_id].quantity=parseInt(cartItem[food_id].quantity)+1;
+        }
+        this.sub=function (food_id) {
+            if(cartItem[food_id].quantity>1){
+                cartItem[food_id].quantity-=1;
+            }else{
+                delete cartItem[food_id];
+            }
+        }
+    })
+})(angular)
--- webapp/js/shop.js
+++ webapp/js/shop.js
@@ -1,8 +1,20 @@
-$(function(){
-// //    获取商铺id
-//     var shopid=window.localStorage.getItem("shopid");
-// //    发送ajax
-//     ajax("/shop",{"shopid":shopid},function(data){
-//
-//     });
-});
+(function(angular) {
+    var app=angular.module("shopApp",["ele.service.cart"]);
+    app.filter('notnull',function () {
+        return function (cartItem) {
+            var items={};
+            for(var key in cartItem){
+                if(cartItem[key]!=null){
+                    items[key]=cartItem[key];
+                }
+            }
+            return items;
+        }
+    })
+    app.controller("shopCtrl",["$scope","cart",function ($scope,cart) {
+        $scope.cartItem=cart.getCartItem();
+        $scope.addItem=cart.addItem;
+        $scope.add=cart.add;
+        $scope.sub=cart.sub;
+    }]);
+})(angular)
