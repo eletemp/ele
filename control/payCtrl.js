9e04422652e1ffe7d2b794a884fb517447368fe5
shufenHou <2485408442@qq.com>
2018-06-15 18:17:53 +0800

    订单模块

--- control/payCtrl.js
+++ control/payCtrl.js
@@ -1,23 +1,35 @@
 "use strict";
 const payDao=require("../dao/payDao");
-function queryDeliverCost(req,res,shopid){
+function queryDeliverCost(req,res,shopid,callback){
     payDao.queryDeliverCost(shopid,function(results){
-        console.log(results[0]);
-        res.render("pay.ejs",results[0]);
+        callback(results[0]);
+    });
+}
+function queryAddress(req,res,userid,callback){
+    payDao.queryAddress(userid,function(results){
+        callback(results[0]);
+    });
+}
+function renderPayUI(req,res,shopid,userid){
+    var array={};
+    queryDeliverCost(req,res,shopid,function(results1){
+        let address=queryAddress(req,res,userid,function(results2){
+            array={deliver:results1,address:results2};
+            res.render("pay.ejs",array);
+        });
     });
 }
 function addAddress(fields,res){
     payDao.addAddress(fields,function(results){
-        if(results!=null){
-        //    查询地址
-            payDao.queryAddress(1,function(results){
-                let address=results[results.length-1];
-                console.log(address);
-                res.render("pay.ejs",address);
-            });
-        }else{
-            res.render("pay.ejs",null);
-        }
+    //    在前台显示添加成功或失败
+        console.log(results);
     });
 }
-module.exports={queryDeliverCost,addAddress};
+function updateAddress(fields,order_id,res){
+    console
+    payDao.updateAddress(fields,order_id,function(results){
+        console.log(results);
+    });
+}
+
+module.exports={renderPayUI,addAddress,updateAddress};
